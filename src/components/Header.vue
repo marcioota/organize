ebc317f83d6b81df614cacda889b3f5ca79a750a
marcioota <marcio.ota@esystem.com.br>
2022-05-01 21:37:47 -0300

    Sprint 2-fase 3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <header>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <nav class="container">
             <router-link to="/" >
